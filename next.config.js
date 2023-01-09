71a01e2894a87960ffe2a6edebe3e55212eb7a0f
Christina Heidt <christina.heidt@digitecgalaxus.ch>
2023-01-09 12:36:39 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,46 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@linaria/webpack-loader",
+          options: {
+            sourceMap: process.env.NODE_ENV !== "production",
+          },
+        },
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+        },
+        {
+          loader: "css-loader",
+          options: {
+            sourceMap: process.env.NODE_ENV !== "production",
+          },
+        },
+      ],
+    });
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "linaria.css",
+      })
+    );
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
